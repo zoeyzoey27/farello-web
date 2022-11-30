a007fbaf66eaad2102d9bb539835f1903f2b0777
loanhtp <loanhtp@rikkeisoft.com>
2022-11-30 14:32:07 +0700

    fix: fix menu admin

--- src/components/admin/MenuAdmin/index.jsx
+++ src/components/admin/MenuAdmin/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Menu, Row, Divider } from 'antd'
 import './style.css'
 import { useNavigate, useLocation } from 'react-router-dom'
@@ -13,16 +13,52 @@ import i18n from '../../../translation'
 const MenuAdmin = () => {
     const navigate = useNavigate()
     const location = useLocation()
+    const [openKeys, setOpenKeys] = useState([])
     const logout = () => {
         localStorage.removeItem("token_admin")
         localStorage.removeItem("id_token_admin")
         navigate("/admin/login")
     }
     const id = localStorage.getItem("id_token_admin")
+    useEffect(() => {
+       switch (location.pathname){
+         case '/admin/categoryManagement':
+            setOpenKeys(['sub1']);
+            break;
+         case '/admin/addCategory':
+            setOpenKeys(['sub1']);
+            break;
+         case '/admin/productManagement' || '/admin/addProduct':
+            setOpenKeys(['sub2']);
+            break;
+         case '/admin/addProduct':
+            setOpenKeys(['sub2']);
+            break;
+         case '/admin/postManagement':
+            setOpenKeys(['sub3']);
+            break;
+         case '/admin/postCategory':
+            setOpenKeys(['sub3']);
+            break;
+         case '/admin/addPost':
+            setOpenKeys(['sub3']);
+            break;
+         case '/admin/adminList':
+            setOpenKeys(['sub4']);
+            break;
+         case '/admin/addAdmin':
+            setOpenKeys(['sub4']);
+            break;
+         default:
+            setOpenKeys([]);
+       }
+    },[location.pathname])
     return (
         <Menu
             selectedKeys={[location.pathname]}
             mode="inline"
+            openKeys={openKeys}
+            onOpenChange={(keys) => {setOpenKeys(keys)}}
             className="h-full w-full py-10 text-[1.6rem] font-semibold text-[#828282]">
             <Menu.Item key="/admin/dashboard" onClick={() => navigate('/admin/dashboard')}>
                 <Row className="flex items-center">
